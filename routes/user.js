33bf6030598793f0d4ff609eab2f0f8866fabe4b
cuijiangtao <861159928@qq.com>
2018-01-08 18:50:56 +0800

    aaa

---
+++ routes/user.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+	host:'localhost',   
+	user:'root',		
+	password:'123456',	
+	database:'list',	
+	port:3306			
+})
+
+router.post('/add',function(req,res){
+ var json1=req.body;
+    pool.getConnection(function(err,connection){
+        if(err) throw err;
+        connection.query(`SELECT * FROM user WHERE user='${json1.user}'`,function(err,rows){
+            if(err) throw err;
+             var data=rows;
+            if(rows.length == 0){
+                connection.query(`INSERT INTO user (user,pass) VALUES ('${json1.user}','${json1.pass}')`,function(err){
+                    if(err) throw err;
+                    connection.query('SELECT * FROM user',function(err,rows){
+                                if(err) throw err;
+                    res.send('注册成功')
+
+                    })
+                
+                   
+                })
+            }else {
+            res.send('用户名已被注册')
+        }
+            
+        })
+    })
+})
+router.post('/dl',function(req,res){
+      var json1=req.body;
+    pool.getConnection(function(err,connection){
+        if(err) throw err;
+        connection.query(`SELECT * FROM user WHERE user='${json1.user}' AND pass='${json1.pass}'`,function(err,rows){
+            if(err) throw err;
+            if(rows.length == 0){
+                 res.send('账号或者密码错误') 
+             }else{
+                  res.send('登录成功')
+             }
+            res.send(rows);
+            connection.release();
+        })
+    
+    })
+})
+
+
+
+
+module.exports=router;
